aafeea06e74a102ee08cb53bad92a26de02184e8
vasek.p <vasek.p@st-software.com>
2014-09-19 09:10:08 +0200

    SgDate updated to support min,max

---
+++ libs/Pickadate.js
@@ -0,0 +1,16 @@
+/// <reference path="../interfaces/IPickadate.d.ts" />
+var Pickadate;
+(function (Pickadate) {
+    //based on http://amsul.ca/pickadate.js/api.htm#method-set
+    Pickadate.pickadateSetThing = {
+        clear: 'clear',
+        select: 'select',
+        highlight: 'highlight',
+        view: 'view',
+        min: 'min',
+        max: 'max',
+        disable: 'disable',
+        enable: 'enable',
+        interval: 'interval'
+    };
+})(Pickadate || (Pickadate = {}));
--- src/directives/SgDate.ts
+++ src/directives/SgDate.ts
@@ -7,10 +7,11 @@ module STAngular {
     //You can specify min|max attributes using moment.js syntax
     //See method parseDateAttr() in file pickadateHelper.ts for details
 
-    Module.directive("sgDate", [<any>"$window", "$timeout", ($window, $timeout: ng.ITimeoutService) => {
+    Module.directive("sgDate", [<any>"$window", "$parse", ($window, $parse: ng.IParseService) => {
         
         return {
             require: "^ngModel",
+            priority: 0,
             restrict: "A",
             link: (scope:ng.IScope, elm, attrs, ctrl:ng.INgModelController) => {
                 var moment = $window.moment;
@@ -19,9 +20,25 @@ module STAngular {
                 var modelValue = scope.$eval(attrs.ngModel);
                 var utcDate = modelValue != null ? moment.utc(modelValue) : null;
 
-                if (attrs.withoutIcon == undefined) {
-                    var pickadateHelper = new STUtils.PickadateHelper();
-                    var datepicker: Pickadate.IPickadate = pickadateHelper.bindPickadateForAngular(elm, utcDate, dateFormat, scope, ctrl);
+				if (!attrs.withoutIcon) {
+					var pickadateHelper = new PickadateHelper();
+					pickadateHelper.bindPickadateForAngular(elm, utcDate, dateFormat, scope, ctrl, $parse);
+				}
+                var isDateValid = (value: Date) : boolean => {
+                    var isValid = true;
+
+                    var min = attrs['min'] != null ? pickadateHelper.parseDateAttr(attrs['min'], scope, $parse, true) : null,
+                        max = attrs['max'] != null ? pickadateHelper.parseDateAttr(attrs['max'], scope, $parse) : null;
+
+                    if (min != null && min > value) {
+                        isValid = false;
+                    }
+
+                    if (max != null && max < value) {
+                        isValid = false;
+                    }
+
+                    return isValid;
                 }
                 
                 attrs.$observe("sgDate", newValue => {
@@ -29,13 +46,14 @@ module STAngular {
                     dateFormat = newValue;
                     ctrl.$modelValue = new Date(<any>ctrl.$setViewValue);
                 });
-
+                
                 ctrl.$formatters.unshift(modelValue => {
                     if (!dateFormat || !modelValue) return "";
                     var utcDate = moment.utc(modelValue);
                     var retVal = utcDate.format(dateFormat);
                     var isValid: boolean = utcDate.isValid();
-                    ctrl.$setValidity(attrs.name, isValid);
+                    ctrl.$setValidity(attrs.name, isValid && isDateValid(utcDate));
+
                     return retVal;
                 });
 
@@ -43,13 +61,14 @@ module STAngular {
                     var date = moment.utc(viewValue, dateFormat);                    
                     var isEmpty = viewValue == null || viewValue.trim() === "";
                     var isRequired = attrs.required;
-                    if (isEmpty && isRequired || (!!date && (date.isValid() == false || date.year() < 1900))) 
+                    if (isEmpty && isRequired || !isEmpty && (!!date && (date.isValid() == false || date.year() < 1900))) 
                     {                        
                         ctrl.$setValidity(attrs.name, false);
                         return "";
                     } else {
-                        ctrl.$setValidity(attrs.name, true);
+                        ctrl.$setValidity(attrs.name, true && isDateValid(date));
                     }
+
                     return !!date ? date.format() : null;
                 });
             }
