822b2a8db869a055e9181d920583b4dd68ca8876
David Votrubec <david.votrubec@gmail.com>
2015-04-17 08:34:32 +0200

    Added support for attribute sg-readonly-ignore
    Elements having attribute sg-readonly-ignore will not be affected by sgReadonly directive

--- src/directives/SgReadonly.ts
+++ src/directives/SgReadonly.ts
@@ -3,6 +3,8 @@
 
 module STAngular {
     
+    ///Finds all contained input elements and add property disabled when condition is met.
+    ///Fields with attribute [sg-readonly-ignore] are not affected
     Module.directive("sgReadonly", [() => {
 
         function toggleDisableAttr(fields:any[], isDisabled:boolean) {
@@ -15,13 +17,19 @@ module STAngular {
 
                 if (attrs.sgReadonlyCacheFields == true || attrs.sgReadonlyCacheFields == "true") {
                     //Caching fields has better performance but does not see fields added later via ng-if etc, so use it only when you are sure that no fields will be added dynamically 
-                    var cachedFields = elm.find('input[ng-model], select[ng-model], textarea[ng-model]').not('[ng-disabled], [ng-readonly], [disabled], [readonly]');
+                    var cachedFields = elm
+                        .find('input[ng-model], select[ng-model], textarea[ng-model]')
+                        .not('[ng-disabled], [ng-readonly], [disabled], [readonly], [sg-readonly-ignore]');
+
                     scope.$watch(attrs.sgReadonly, (value: boolean) => {
                         toggleDisableAttr(cachedFields, value);
                     });
                 } else {
                     scope.$watch(attrs.sgReadonly, (value: boolean) => {
-                        var fields = elm.find('input[ng-model], select[ng-model], textarea[ng-model]').not('[ng-disabled], [ng-readonly], [disabled], [readonly]');
+                        var fields = elm
+                            .find('input[ng-model], select[ng-model], textarea[ng-model]')
+                            .not('[ng-disabled], [ng-readonly], [sg-readonly-ignore]');
+
                         toggleDisableAttr(fields, value);
                     });    
                 }
