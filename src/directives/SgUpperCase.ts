9ec48f670f15dbb8fb7c815de973cd90a9dc17ef
David Votrubec <david.votrubec@gmail.com>
2015-05-07 12:17:47 +0200

    Fixed bug with cursor jumping at the end

--- src/directives/SgUpperCase.ts
+++ src/directives/SgUpperCase.ts
@@ -5,6 +5,46 @@ module STAngular {
 
     Module.directive("sgUpperCase", [() => {
         
+        function getCaretPosition(inputField: HTMLInputElement): number {
+            // Initialize
+            var position = 0;
+
+            // IE Support
+            if (document.selection) {
+                inputField.focus();
+
+                // To get cursor position, get empty selection range
+                var emptySelection = document.selection.createRange();
+
+                // Move selection start to 0 position
+                emptySelection.moveStart('character', -inputField.value.length);
+
+                // The caret position is selection length
+                position = emptySelection.text.length;
+            }
+            else if (inputField.selectionStart || inputField.selectionStart == 0) {
+                position = inputField.selectionStart;
+            }
+
+            return position;
+        }
+
+        function setCaretPosition(inputElement: HTMLInputElement, position: number) {
+            if (inputElement.createTextRange) {
+                var range = inputElement.createTextRange();
+                range.move('character', position);
+                range.select();
+            }
+            else {
+                if (inputElement.selectionStart) {
+                    inputElement.focus();
+                    inputElement.setSelectionRange(position, position);
+                } else {
+                    inputElement.focus();
+                }
+            }
+        }
+
         return {
             require: "^ngModel",
             restrict: "A",
@@ -14,8 +54,12 @@ module STAngular {
 
                     var modified = inputValue.toUpperCase();
                     if (modified !== inputValue) {
+                        var position = getCaretPosition(<HTMLInputElement>elm[0]);
+
                         ctrl.$setViewValue(modified);
                         ctrl.$render();
+
+                        setCaretPosition(<HTMLInputElement>elm[0], position);
                     }
                     return modified;
                 }
